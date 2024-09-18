718892cb542d4a8032e45d27e0ef22ad97ec1f3e
CauanTEC <104176406+CauanTEC@users.noreply.github.com>
2024-09-17 21:18:43 -0300

    alterado

--- dados.js
+++ dados.js
@@ -2,25 +2,25 @@ let carros = [
     {
         titulo: "Gol Quadrado",
         descricao: "O Gol Quadrado foi um dos carros mais populares e icônicos da história automobilística brasileira. Produzido pela Volkswagen, ele marcou uma época e conquistou o coração de milhões de brasileiros.",
-        img: "/home/cauantec/Documentos/google/Imagens/gol.jpg",
+        img: "/Imagens/gol.jpg",
         link: "https://pt.wikipedia.org/wiki/wiki/Volkswagen_Gol#Primeira_gera%C3%A7%C3%A3o"
     },
     {
         titulo: "Voyage Quadrada",
         descricao: "O Voyage Quadrado foi outro ícone da Volkswagen, conhecido por sua versatilidade e conforto. Sucedendo o Passat, o Voyage Quadrado oferecia amplo espaço interno e diversas opções de motorização.",
-        img: "/home/cauantec/Documentos/google/Imagens/voyage.jpg",
+        img: "/Imagens/voyage.jpg",
         link: "https://pt.wikipedia.org/wiki/wiki/Volkswagen_Voyage"
     },
     {
         titulo: "Saveiro Quadrada",
         descricao: "A Saveiro Quadrada era a picape compacta da Volkswagen,ideal para o trabalho e o lazer. Robusta e versátil, a Saveiro Quadrada oferecia diversas opções de caçamba e motorização. Seus atributos como robustez, baixo custo de manutenção e facilidade de adaptação a diferentes tipos de trabalho a tornaram um grande sucesso de vendas.",
-        img: "/home/cauantec/Documentos/google/Imagens/saveiro.jpg",
+        img: "/Imagens/saveiro.jpg",
         link: "https://pt.wikipedia.org/wiki/wiki/Volkswagen_Saveiro"
     },
     {
         titulo: "Parati Quadrada",
         descricao: "A Parati Quadrada era a perua da família Volkswagen,conhecida por seu amplo espaço interno e versatilidade. Ideal para famíliase para o transporte de cargas, a Parati Quadrada oferecia diversas configurações de assentos e porta-malas. Sua suspensão macia e dirigibilidade confortável a tornaram uma opção popular para o dia a dia.",
-        img: "/home/cauantec/Documentos/google/Imagens/parati.jpg",
+        img: "/Imagens/parati.jpg",
         link: "https://pt.wikipedia.org/wiki/wiki/Volkswagen_Parati"
     }
 ];
